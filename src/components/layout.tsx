bdd92a2e5dbb661d0b8d928a622aee76ba9cf5b1
elvisyong <elvisyong@gmail.com>
2023-10-28 01:20:31 +0800

    refactor: add types to layour

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,13 @@
+import { Toaster } from "react-hot-toast";
 import NavBar from "./nav/Nav";
 
 
-export default function Layout({ children }) {
+export default function Layout({ children: children }: { children: React.ReactNode }) {
   return (
     <>
+      <div><Toaster
+        position="top-right"
+      /></div>
       <NavBar />
       <div>
         {children}
